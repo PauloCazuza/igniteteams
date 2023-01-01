6ad3acc48fd5bae41b778a67cd3f31ea6096a507
PauloCazuza <prlopes_@hotmail.com>
2023-01-01 18:35:13 -0300

    add font roboto to app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { ThemeProvider } from "styled-components"
 import Groups from "@screens/Groups";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
     <ThemeProvider theme={theme} >
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />
+      }
     </ThemeProvider>
   );
 }
