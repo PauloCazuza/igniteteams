6e887b90c2b639ddf3032c98f9ec721750ad0527
PauloCazuza <prlopes_@hotmail.com>
2023-01-02 15:52:35 -0300

    add list group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import GroupCard from "@components/GroupCard";
 import Header from "@components/Header";
 import HighLight from "@components/Highlight";
 import { Container } from "./styles";
 
 export default function Groups() {
+    const [groups, setGroups] = useState<string[]>(["Galera da Rocket", "Amigos"]);
+
     return (
         <Container>
             <Header />
 
             <HighLight title="Turmas" subtitle="Jogue com sua turma " />
-            <GroupCard title="Galera do Ignite" />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+            />
         </Container>
     );
 }
