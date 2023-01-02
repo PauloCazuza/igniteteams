8ef042f66bceeda3a75464d0fdeb6e14a64b582e
PauloCazuza <prlopes_@hotmail.com>
2023-01-02 11:48:58 -0300

    create component loading and create type to theme

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import Groups from "@screens/Groups";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
-import { ActivityIndicator } from "react-native";
+import Loading from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,8 +13,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme} >
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />
-      }
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
