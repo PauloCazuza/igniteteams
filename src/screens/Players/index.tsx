6a146cb0c0cee328aa85566728db7c0ed05a1943
PauloCazuza <prlopes_@hotmail.com>
2023-01-03 18:22:27 -0300

    create list card team horizontal

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import ButtonIcon from "@components/ButtonIcon";
 import Filter from "@components/Filter";
 import Header from "@components/Header";
 import HighLight from "@components/Highlight";
 import Input from "@components/Inputs";
-import { Container, Form } from "./styles";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 function Players() {
+    const [team, setTeam] = useState("Time A");
+    const [players, setPlayers] = useState(["Player 1", "Player 2"]);
+
     return (
         <Container>
             <Header showBackButton />
@@ -26,10 +33,27 @@ function Players() {
                 />
             </Form>
 
-            <Filter
-                title="Time A"
-                isActive
-            />
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => {
+                        return (
+                            <Filter
+                                title={item}
+                                isActive={item === team}
+                                onPress={() => setTeam(item)}
+                            />
+                        )
+                    }}
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+            </HeaderList>
+
         </Container>
     );
 }
